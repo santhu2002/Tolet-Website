9427d3e11c368e9336b9ce72785e6d8db9f71b93
Santhu <purisanthosh2002@gmail.com>
2023-01-18 23:32:15 +0530

    Nav Updated

--- Houses/views.py
+++ Houses/views.py
@@ -5,16 +5,22 @@ from Home.models import Enterhome
 
 # Create your views here.
 def enterhome(request):
-    if(request.method == 'POST'):
-        name = request.POST['name']
-        desc = request.POST['desc']
-        location = request.POST['location']
-        price = request.POST['price']
-        offer =request.POST.get('offer',False)
-        img = request.FILES['img']
-        en=Enterhome(name=name,desc=desc,location=location,price=price,offer=offer,img=img)
-        en.save()
-    return render(request,'Enterhome.html')
+    if request.user.is_authenticated:
+        if(request.method == 'POST'):
+            name = request.POST['name']
+            desc = request.POST['desc']
+            location = request.POST['location']
+            price = request.POST['price']
+            offer =request.POST.get('offer',False)
+            img = request.FILES['img']
+            en=Enterhome(name=name,desc=desc,location=location,price=price,offer=offer,img=img)
+            en.save()
+        return render(request,'Enterhome.html')
+    else:
+        return redirect('/accounts/login')
 def ourhomes(request):
-    homes=Enterhome.objects.all()
-    return render(request,'Ourhomes.html',{'homes':homes})
+    if request.user.is_authenticated:
+        homes=Enterhome.objects.all()
+        return render(request,'Ourhomes.html',{'homes':homes})
+    else:
+        return redirect('/accounts/login')
